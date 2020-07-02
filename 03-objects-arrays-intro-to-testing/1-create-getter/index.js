145c6b11d0ccd172829d922d0ddede4def05fe9f
maximusnet <a4UmNlwVNGp>
2020-07-02 14:32:59 +0600

    3 урок

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,20 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
+//Вариант так себе - кривой, в комента через eval
+  export function createGetter(path) {
+    let patharr = path.split("."); //путь
+    let fooGetter = function (obj){
+      if (Object.keys(obj).length){ //пустой или нет
+        switch (patharr.length){// выбираем по пути
+        case 1 :
+          return obj[patharr[0]];
+        case 2 :
+          return obj[patharr[0]][patharr[1]];
+        case 3 :
+          return obj[patharr[0]][patharr[1]][patharr[2]];
+        }}
+    };
+    return fooGetter;
+  }
 
-}
--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,22 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  let Sum = [];
+  const initValuel=0;
+  const objSymbol={};
+  const size = 2;
+  if(!arr) { return []; }
 
+  arr.map((elem)=>{
+    if(elem in objSymbol) {objSymbol[elem] +=1;}
+    else {objSymbol[elem]=1;}
+    if(objSymbol[elem]<size){
+      return Sum.push(elem);
+    }
+    else{
+      return Sum;
+    }
+  },initValuel);
+ // console.log(Sum);
+  return Sum;
 }
