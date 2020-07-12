40adc20bea1dd1fd83d1ecf689543b53a2f56470
autotechkaz@gmail.com <a4UmNlwVNGp>
2020-07-12 19:48:16 +0600

    5.1 не понятно почему не проходит тест

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,59 @@
 export default class NotificationMessage {
+  static active;
+  message = null
+  durationInSeconds = 2;
+  type = "";
+  duration = 2000;
+  element =null;
+  timerId = null;
 
-}
+  constructor(message,{duration = 2000,
+    type = 'success',}={}){
+    this.message=message;
+    this.durationInSeconds=(duration/1000)+ 's';;
+    this.duration=duration;
+    this.type=type;
+
+    if (NotificationMessage.active) {
+      NotificationMessage.active.remove();
+    }
+    this.render();
+
+  }
+  render(){
+    const element = document.createElement(`div`);
+    element.innerHTML=`<div class="notification ${this.type}" style="--value:${this.durationInSeconds}">
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+        <div class="notification-header">Notification</div>
+        <div class="notification-body">
+          ${this.message}
+        </div>
+      </div>
+    </div>`;
+    this.element = element.firstElementChild;
+    NotificationMessage.active = this.element;
+
+  }
+  destroy() {
+    this.element.remove();
+    NotificationMessage.active=null;
+  }
+  show(parent = document.body) {
+    console.log(`Before parent append - `, parent);
+    parent.append(this.element);
+    console.log(`After element append - `,this.element.outerHTML);
+    console.log(`After parent append - ` ,parent.outerHTML);
+    this.timerId = setTimeout(this.remove(),this.duration);
+    clearTimeout(this.timerId);
+    return this.element;
+  }
+  remove() {
+  this.element.remove();
+  this.element=null;
+  NotificationMessage.active=null;
+
+  }
+
+
+};
